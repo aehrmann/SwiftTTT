cd10e149bcb5621adeece2e87c0c835fdfda50f4
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-07-15 16:37:23 -0500

    WIP BasicRules

--- SwiftTTT/BasicRules.swift
+++ SwiftTTT/BasicRules.swift
@@ -2,5 +2,72 @@ public class BasicRules {
     public let player = "X"
     public let opponent = "O"
     
+    private let winningXSet = "XXX"
+    
     public init() {}
+    
+    public func playerWins(board: String) -> Bool {
+        
+        if any(self.rows(board), predicate: { self.isWinningXSet($0) }) {
+            return true
+        }
+        if any(self.columns(board), predicate: { self.isWinningXSet($0) }) {
+            return true
+        }
+        
+        if board[0] == "X" && board[4] == "X" && board[8] == "X" {
+            return true
+        }
+        if board[0] == "X" && board[4] == "X" && board[8] == "X" {
+            return true
+        }
+
+        return false
+    }
+    
+    private func isWinningXSet(s: String) -> Bool {
+        return s == self.winningXSet
+    }
+    
+    private func any(strings: [String], predicate: String -> Bool) -> Bool {
+        for s in strings {
+            if predicate(s) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func rows(board: String) -> [String] {
+        return Array(0..<3).map { return self.row($0, of: board) }
+    }
+    
+    private func columns(board: String) -> [String] {
+        return Array(0..<3).map { return self.column($0, of: board) }
+    }
+    
+    private func row(n: Int, of board: String) -> String {
+        let start = n * 3
+        let end = (n + 1) * 3
+        return board[start..<end]
+    }
+    
+    private func column(n: Int, of board: String) -> String {
+        var substring = ""
+        for i in 0..<count(board) {
+            if i % 3 == n {
+                substring += board[i]
+            }
+        }
+        return substring
+    }
+}
+
+extension String {
+    subscript (i: Int) -> String {
+        return String(self[advance(startIndex, i)])
+    }
+    subscript(range: Range<Int>) -> String {
+        return self[advance(startIndex, range.startIndex)..<advance(startIndex, range.endIndex)]
+    }
 }
--- SwiftTTTSpecs/BasicRulesSpec.swift
+++ SwiftTTTSpecs/BasicRulesSpec.swift
@@ -6,11 +6,77 @@ public class BasicRulesSpec: QuickSpec {
     override public func spec() {
         describe("BasicRules") {
             
+            let rules = BasicRules()
+            
             describe("Creating a set of rules") {
                 it("stores player and opponent marks") {
-                    let rules = BasicRules()
                     expect(rules.player).to(equal("X"))
                     expect(rules.opponent).to(equal("O"))
+                }
+            }
+            
+            describe("Checking for a winner") {
+                describe("when the user wins") {
+                    context("in the first row") {
+                        it("returns true") {
+                            let board = "XXX______"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+                    context("in the second row") {
+                        it("returns true") {
+                            let board = "___XXX___"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+
+                    context("in the third row") {
+                        it("returns true") {
+                            let board = "______XXX"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+
+                    context("in the first column") {
+                        it("returns true") {
+                            let board = "X__X__X__"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+                    context("in the second column") {
+                        it("returns true") {
+                            let board = "_X__X__X _"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+                    
+                    context("in the third column") {
+                        it("returns true") {
+                            let board = "__X__X__X"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+
+                    context("in the left to right diagonal") {
+                        it("returns true") {
+                            let board = "X___X___X"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+
+                    context("in the right to left diagonal") {
+                        it("returns true") {
+                            let board = "__X_X_X__"
+                            expect(rules.playerWins(board)).to(beTrue())
+                        }
+                    }
+
+                }
+                describe("when the user does not win") {
+                    it("returns false") {
+                        let board = "_________"
+                        expect(rules.playerWins(board)).to(beFalse())
+                    }
                 }
             }
         }
