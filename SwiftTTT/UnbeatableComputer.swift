dcda6e97be8f26c3a1f7d8b64665a83f435111c0
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-07-15 17:00:27 -0500

    Add logic to UnbeatableComputer to win or block on next move

--- SwiftTTT/UnbeatableComputer.swift
+++ SwiftTTT/UnbeatableComputer.swift
@@ -2,4 +2,35 @@ public class UnbeatableComputer {
     public let mark: Mark = .O
     
     public init() { }
+    
+    public func nextMove(board: MutableBoard) -> Int {
+        var openPositions = Array(0..<9).filter { board.isBlank($0) }
+        var nextBoard: MutableBoard!
+        let rules = Rules()
+        
+        for position in openPositions {
+            nextBoard = createNextBoard(with: self.mark, at: position, on: board)
+            
+            if rules.markIsWinner(self.mark, of: nextBoard) {
+                return position
+            }
+            
+            nextBoard = createNextBoard(with: .X, at: position, on: board)
+            
+            if rules.markIsWinner(.X, of: nextBoard) {
+                return position
+            }
+        }
+        return 0
+    }
+    
+    private func createNextBoard(with mark: Mark, at position: Int, on board: MutableBoard) -> MutableBoard {
+        var nextBoard = MutableBoard()
+        for i in 0..<9 {
+            nextBoard.placeMark(board.markAt(i), at: i)
+        }
+        nextBoard.placeMark(mark, at: position)
+        return nextBoard
+    }
+    
 }
--- SwiftTTTSpecs/UnbeatableComputerSpec.swift
+++ SwiftTTTSpecs/UnbeatableComputerSpec.swift
@@ -5,11 +5,23 @@ import SwiftTTT
 public class UnbeatableComputerSpec: QuickSpec {
     override public func spec() {
         describe("UnbeatableComputer") {
+            let computer = UnbeatableComputer()
             
             describe("Creating an unbeatable computer") {
                 it("stores O as its mark") {
-                    let computer = UnbeatableComputer()
                     expect(computer.mark).to(equal(Mark.O))
+                }
+            }
+            
+            describe("Choosing the next move") {
+                it("wins when it can") {
+                    let board = mutableBoardWithMarksAtPositions([.O: [0, 1]])
+                    expect(computer.nextMove(board)).to(equal(2))
+                }
+                
+                it("blocks an opponent's win") {
+                    let board = mutableBoardWithMarksAtPositions([.X: [3, 4]])
+                    expect(computer.nextMove(board)).to(equal(5))
                 }
             }
         }
